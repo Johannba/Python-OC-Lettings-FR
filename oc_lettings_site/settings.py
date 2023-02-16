f37d1e633652217fdc21c67791ef2b41833ead57
Johannba <johannbacha@gmail.com>
2023-02-16 17:30:02 +0100

    Flake8 correction

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -117,19 +117,16 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 
-
 sentry_sdk.init(
     dsn=env("SENTRY_SDK"),
     integrations=[
         DjangoIntegration(),
     ],
-
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    send_default_pii=True,
 )
