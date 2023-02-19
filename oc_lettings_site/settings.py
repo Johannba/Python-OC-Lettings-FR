29e823304c4d4e6f7d89363e700158c7afbd31cc
Johannba <johannbacha@gmail.com>
2023-02-19 17:48:58 +0100

    config allowed

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "oc-lettings-johannb.herokuapp.com"]
+ALLOWED_HOSTS = ["0.0.0.0", "oc-lettings-johannb.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
