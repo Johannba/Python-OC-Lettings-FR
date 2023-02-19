7948c5e9a5e8e4233bac2fc7adc335ca96811bf7
Johannba <johannbacha@gmail.com>
2023-02-19 18:23:30 +0100

    fix bug

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -117,16 +117,17 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 
-sentry_sdk.init(
-    dsn=os.environ.get("SENTRY_SDK", default="sentry_sdk"),
-    integrations=[
-        DjangoIntegration(),
-    ],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
+if "SENTRY_SDK" in os.environ:
+    sentry_sdk.init(
+        dsn=os.environ.get("SENTRY_SDK"),
+        integrations=[
+            DjangoIntegration(),
+        ],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
